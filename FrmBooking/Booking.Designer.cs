02878b01d19387891c8b18f22d124f170b00c6d4
dane brown <oby_dude@hotmail.com>
2026-04-03 22:33:26 -0500

    >removed and realigned booking form >deleted and recreated branch "applications/features-dane >added booking form features

---
+++ FrmBooking/Booking.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace FrmBooking
+{
+    partial class Booking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblNewPatient = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblDob = new System.Windows.Forms.Label();
+            this.dtpDob = new System.Windows.Forms.DateTimePicker();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.radioYes = new System.Windows.Forms.RadioButton();
+            this.radioNo = new System.Windows.Forms.RadioButton();
+            this.lblAppDate = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnBook = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(66, 22);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(369, 37);
+            this.lblHeading.TabIndex = 0;
+            this.lblHeading.Text = "Request an Appointment";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(48, 103);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(48, 138);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblNewPatient
+            // 
+            this.lblNewPatient.AutoSize = true;
+            this.lblNewPatient.Location = new System.Drawing.Point(48, 182);
+            this.lblNewPatient.Name = "lblNewPatient";
+            this.lblNewPatient.Size = new System.Drawing.Size(110, 13);
+            this.lblNewPatient.TabIndex = 3;
+            this.lblNewPatient.Text = "Are you a new patient";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(48, 271);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(78, 13);
+            this.lblPhone.TabIndex = 4;
+            this.lblPhone.Text = "Phone Number";
+            // 
+            // lblDob
+            // 
+            this.lblDob.AutoSize = true;
+            this.lblDob.Location = new System.Drawing.Point(48, 235);
+            this.lblDob.Name = "lblDob";
+            this.lblDob.Size = new System.Drawing.Size(66, 13);
+            this.lblDob.TabIndex = 5;
+            this.lblDob.Text = "Date of Birth";
+            // 
+            // dtpDob
+            // 
+            this.dtpDob.Location = new System.Drawing.Point(281, 227);
+            this.dtpDob.Name = "dtpDob";
+            this.dtpDob.Size = new System.Drawing.Size(200, 20);
+            this.dtpDob.TabIndex = 6;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(281, 100);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(95, 20);
+            this.txtFirstName.TabIndex = 7;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(386, 100);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(95, 20);
+            this.txtLastName.TabIndex = 8;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(281, 131);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(200, 20);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(281, 264);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(200, 20);
+            this.txtPhone.TabIndex = 10;
+            this.txtPhone.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // radioYes
+            // 
+            this.radioYes.AutoSize = true;
+            this.radioYes.Location = new System.Drawing.Point(281, 178);
+            this.radioYes.Name = "radioYes";
+            this.radioYes.Size = new System.Drawing.Size(43, 17);
+            this.radioYes.TabIndex = 11;
+            this.radioYes.TabStop = true;
+            this.radioYes.Text = "Yes";
+            this.radioYes.UseVisualStyleBackColor = true;
+            // 
+            // radioNo
+            // 
+            this.radioNo.AutoSize = true;
+            this.radioNo.Location = new System.Drawing.Point(372, 178);
+            this.radioNo.Name = "radioNo";
+            this.radioNo.Size = new System.Drawing.Size(39, 17);
+            this.radioNo.TabIndex = 12;
+            this.radioNo.TabStop = true;
+            this.radioNo.Text = "No";
+            this.radioNo.UseVisualStyleBackColor = true;
+            // 
+            // lblAppDate
+            // 
+            this.lblAppDate.AutoSize = true;
+            this.lblAppDate.Location = new System.Drawing.Point(48, 306);
+            this.lblAppDate.Name = "lblAppDate";
+            this.lblAppDate.Size = new System.Drawing.Size(92, 13);
+            this.lblAppDate.TabIndex = 13;
+            this.lblAppDate.Text = "Appointment Date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(281, 298);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 14;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(281, 378);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(50, 23);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnBook
+            // 
+            this.btnBook.Location = new System.Drawing.Point(431, 378);
+            this.btnBook.Name = "btnBook";
+            this.btnBook.Size = new System.Drawing.Size(50, 23);
+            this.btnBook.TabIndex = 16;
+            this.btnBook.Text = "Book";
+            this.btnBook.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(353, 378);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(50, 23);
+            this.btnCancel.TabIndex = 17;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // Booking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 470);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnBook);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblAppDate);
+            this.Controls.Add(this.radioNo);
+            this.Controls.Add(this.radioYes);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.dtpDob);
+            this.Controls.Add(this.lblDob);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblNewPatient);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblHeading);
+            this.Name = "Booking";
+            this.Text = "Booking";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblNewPatient;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblDob;
+        private System.Windows.Forms.DateTimePicker dtpDob;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.RadioButton radioYes;
+        private System.Windows.Forms.RadioButton radioNo;
+        private System.Windows.Forms.Label lblAppDate;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnBook;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ FrmBooking/Booking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmBooking
+{
+    public partial class Booking : Form
+    {
+        public Booking()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
