656ad8005a45199c8ed57facbf312490be7c92ec
dane brown <oby_dude@hotmail.com>
2026-04-20 10:46:41 -0500

    started new branch created first user management portal form updated validation fixed text enter arguments

---
+++ UserMgmt/FrmNewAppt.Designer.cs
@@ -0,0 +1,414 @@
+﻿namespace UserMgmt
+{
+    partial class FrmNewAppt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewAppt = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblParish = new System.Windows.Forms.Label();
+            this.lblTown = new System.Windows.Forms.Label();
+            this.txtTown = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblLastN = new System.Windows.Forms.Label();
+            this.lblFirstN = new System.Windows.Forms.Label();
+            this.radioFemale = new System.Windows.Forms.RadioButton();
+            this.radioMale = new System.Windows.Forms.RadioButton();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblMobile = new System.Windows.Forms.Label();
+            this.lblCell = new System.Windows.Forms.Label();
+            this.txtMobile = new System.Windows.Forms.TextBox();
+            this.txtCell = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblDob = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblHeadingNewAppt = new System.Windows.Forms.Label();
+            this.cmbDay = new System.Windows.Forms.ComboBox();
+            this.cmbMonth = new System.Windows.Forms.ComboBox();
+            this.cmbYear = new System.Windows.Forms.ComboBox();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.lblMonth = new System.Windows.Forms.Label();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.cmbParish = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnNewAppt
+            // 
+            this.btnNewAppt.Location = new System.Drawing.Point(516, 390);
+            this.btnNewAppt.Name = "btnNewAppt";
+            this.btnNewAppt.Size = new System.Drawing.Size(75, 23);
+            this.btnNewAppt.TabIndex = 52;
+            this.btnNewAppt.Text = "New User";
+            this.btnNewAppt.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(409, 390);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 51;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // lblParish
+            // 
+            this.lblParish.AutoSize = true;
+            this.lblParish.Font = new System.Drawing.Font("Segoe Print", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblParish.Location = new System.Drawing.Point(417, 319);
+            this.lblParish.Name = "lblParish";
+            this.lblParish.Size = new System.Drawing.Size(32, 14);
+            this.lblParish.TabIndex = 50;
+            this.lblParish.Text = "Parish";
+            // 
+            // lblTown
+            // 
+            this.lblTown.AutoSize = true;
+            this.lblTown.Font = new System.Drawing.Font("Segoe Print", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTown.Location = new System.Drawing.Point(225, 319);
+            this.lblTown.Name = "lblTown";
+            this.lblTown.Size = new System.Drawing.Size(29, 14);
+            this.lblTown.TabIndex = 48;
+            this.lblTown.Text = "Town";
+            // 
+            // txtTown
+            // 
+            this.txtTown.Location = new System.Drawing.Point(221, 332);
+            this.txtTown.Name = "txtTown";
+            this.txtTown.Size = new System.Drawing.Size(182, 20);
+            this.txtTown.TabIndex = 47;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(221, 291);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(374, 20);
+            this.txtAddress.TabIndex = 46;
+            // 
+            // lblLastN
+            // 
+            this.lblLastN.AutoSize = true;
+            this.lblLastN.Font = new System.Drawing.Font("Segoe Print", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastN.Location = new System.Drawing.Point(417, 95);
+            this.lblLastN.Name = "lblLastN";
+            this.lblLastN.Size = new System.Drawing.Size(24, 14);
+            this.lblLastN.TabIndex = 45;
+            this.lblLastN.Text = "Last";
+            // 
+            // lblFirstN
+            // 
+            this.lblFirstN.AutoSize = true;
+            this.lblFirstN.Font = new System.Drawing.Font("Segoe Print", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstN.Location = new System.Drawing.Point(225, 95);
+            this.lblFirstN.Name = "lblFirstN";
+            this.lblFirstN.Size = new System.Drawing.Size(25, 14);
+            this.lblFirstN.TabIndex = 44;
+            this.lblFirstN.Text = "First";
+            // 
+            // radioFemale
+            // 
+            this.radioFemale.AutoSize = true;
+            this.radioFemale.Location = new System.Drawing.Point(297, 218);
+            this.radioFemale.Name = "radioFemale";
+            this.radioFemale.Size = new System.Drawing.Size(59, 17);
+            this.radioFemale.TabIndex = 42;
+            this.radioFemale.TabStop = true;
+            this.radioFemale.Text = "Female";
+            this.radioFemale.UseVisualStyleBackColor = true;
+            // 
+            // radioMale
+            // 
+            this.radioMale.AutoSize = true;
+            this.radioMale.Location = new System.Drawing.Point(221, 218);
+            this.radioMale.Name = "radioMale";
+            this.radioMale.Size = new System.Drawing.Size(48, 17);
+            this.radioMale.TabIndex = 41;
+            this.radioMale.TabStop = true;
+            this.radioMale.Text = "Male";
+            this.radioMale.UseVisualStyleBackColor = true;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(221, 180);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(229, 20);
+            this.txtEmail.TabIndex = 40;
+            // 
+            // lblMobile
+            // 
+            this.lblMobile.AutoSize = true;
+            this.lblMobile.Location = new System.Drawing.Point(414, 148);
+            this.lblMobile.Name = "lblMobile";
+            this.lblMobile.Size = new System.Drawing.Size(41, 13);
+            this.lblMobile.TabIndex = 39;
+            this.lblMobile.Text = "Mobile:";
+            // 
+            // lblCell
+            // 
+            this.lblCell.AutoSize = true;
+            this.lblCell.Location = new System.Drawing.Point(218, 148);
+            this.lblCell.Name = "lblCell";
+            this.lblCell.Size = new System.Drawing.Size(27, 13);
+            this.lblCell.TabIndex = 38;
+            this.lblCell.Text = "Cell:";
+            // 
+            // txtMobile
+            // 
+            this.txtMobile.Location = new System.Drawing.Point(457, 144);
+            this.txtMobile.Name = "txtMobile";
+            this.txtMobile.Size = new System.Drawing.Size(124, 20);
+            this.txtMobile.TabIndex = 37;
+            // 
+            // txtCell
+            // 
+            this.txtCell.Location = new System.Drawing.Point(248, 144);
+            this.txtCell.Name = "txtCell";
+            this.txtCell.Size = new System.Drawing.Size(124, 20);
+            this.txtCell.TabIndex = 36;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(413, 108);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(182, 20);
+            this.txtLastName.TabIndex = 35;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(221, 108);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(182, 20);
+            this.txtFirstName.TabIndex = 34;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(75, 298);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 33;
+            this.lblAddress.Text = "Address";
+            // 
+            // lblDob
+            // 
+            this.lblDob.AutoSize = true;
+            this.lblDob.Location = new System.Drawing.Point(75, 259);
+            this.lblDob.Name = "lblDob";
+            this.lblDob.Size = new System.Drawing.Size(66, 13);
+            this.lblDob.TabIndex = 32;
+            this.lblDob.Text = "Date of Birth";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(75, 222);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(42, 13);
+            this.lblGender.TabIndex = 31;
+            this.lblGender.Text = "Gender";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(75, 187);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 30;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(75, 151);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(38, 13);
+            this.lblPhone.TabIndex = 29;
+            this.lblPhone.Text = "Phone";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(75, 108);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 28;
+            this.lblName.Text = "Name";
+            // 
+            // lblHeadingNewAppt
+            // 
+            this.lblHeadingNewAppt.AutoSize = true;
+            this.lblHeadingNewAppt.Font = new System.Drawing.Font("Segoe Print", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeadingNewAppt.Location = new System.Drawing.Point(56, 22);
+            this.lblHeadingNewAppt.Name = "lblHeadingNewAppt";
+            this.lblHeadingNewAppt.Size = new System.Drawing.Size(430, 56);
+            this.lblHeadingNewAppt.TabIndex = 27;
+            this.lblHeadingNewAppt.Text = "Request an Appointment";
+            // 
+            // cmbDay
+            // 
+            this.cmbDay.FormattingEnabled = true;
+            this.cmbDay.Location = new System.Drawing.Point(221, 251);
+            this.cmbDay.Name = "cmbDay";
+            this.cmbDay.Size = new System.Drawing.Size(98, 21);
+            this.cmbDay.TabIndex = 53;
+            // 
+            // cmbMonth
+            // 
+            this.cmbMonth.FormattingEnabled = true;
+            this.cmbMonth.Location = new System.Drawing.Point(343, 251);
+            this.cmbMonth.Name = "cmbMonth";
+            this.cmbMonth.Size = new System.Drawing.Size(98, 21);
+            this.cmbMonth.TabIndex = 54;
+            // 
+            // cmbYear
+            // 
+            this.cmbYear.FormattingEnabled = true;
+            this.cmbYear.Location = new System.Drawing.Point(466, 251);
+            this.cmbYear.Name = "cmbYear";
+            this.cmbYear.Size = new System.Drawing.Size(98, 21);
+            this.cmbYear.TabIndex = 55;
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.Font = new System.Drawing.Font("Segoe Print", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDay.Location = new System.Drawing.Point(225, 237);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(23, 14);
+            this.lblDay.TabIndex = 56;
+            this.lblDay.Text = "Day";
+            // 
+            // lblMonth
+            // 
+            this.lblMonth.AutoSize = true;
+            this.lblMonth.Font = new System.Drawing.Font("Segoe Print", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMonth.Location = new System.Drawing.Point(349, 237);
+            this.lblMonth.Name = "lblMonth";
+            this.lblMonth.Size = new System.Drawing.Size(34, 14);
+            this.lblMonth.TabIndex = 57;
+            this.lblMonth.Text = "Month";
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Font = new System.Drawing.Font("Segoe Print", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYear.Location = new System.Drawing.Point(473, 237);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(25, 14);
+            this.lblYear.TabIndex = 58;
+            this.lblYear.Text = "Year";
+            // 
+            // cmbParish
+            // 
+            this.cmbParish.FormattingEnabled = true;
+            this.cmbParish.Location = new System.Drawing.Point(413, 332);
+            this.cmbParish.Name = "cmbParish";
+            this.cmbParish.Size = new System.Drawing.Size(182, 21);
+            this.cmbParish.TabIndex = 59;
+            // 
+            // FrmNewAppt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 481);
+            this.Controls.Add(this.cmbParish);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.lblMonth);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.cmbYear);
+            this.Controls.Add(this.cmbMonth);
+            this.Controls.Add(this.cmbDay);
+            this.Controls.Add(this.btnNewAppt);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblParish);
+            this.Controls.Add(this.lblTown);
+            this.Controls.Add(this.txtTown);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblLastN);
+            this.Controls.Add(this.lblFirstN);
+            this.Controls.Add(this.radioFemale);
+            this.Controls.Add(this.radioMale);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblMobile);
+            this.Controls.Add(this.lblCell);
+            this.Controls.Add(this.txtMobile);
+            this.Controls.Add(this.txtCell);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblDob);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblHeadingNewAppt);
+            this.Name = "FrmNewAppt";
+            this.Text = "FrmNewAppt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewAppt;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblParish;
+        private System.Windows.Forms.Label lblTown;
+        private System.Windows.Forms.TextBox txtTown;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblLastN;
+        private System.Windows.Forms.Label lblFirstN;
+        private System.Windows.Forms.RadioButton radioFemale;
+        private System.Windows.Forms.RadioButton radioMale;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblMobile;
+        private System.Windows.Forms.Label lblCell;
+        private System.Windows.Forms.TextBox txtMobile;
+        private System.Windows.Forms.TextBox txtCell;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblDob;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblHeadingNewAppt;
+        private System.Windows.Forms.ComboBox cmbDay;
+        private System.Windows.Forms.ComboBox cmbMonth;
+        private System.Windows.Forms.ComboBox cmbYear;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.Label lblMonth;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.ComboBox cmbParish;
+    }
+}
---
+++ UserMgmt/FrmPortal.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace UserMgmt
+{
+    public partial class FrmPortal : Form
+    {
+        public FrmPortal()
+        {
+            InitializeComponent();
+        }
+
+        private void lblGender_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool ValidateForm()
+        {
+            // First Name
+            if (string.IsNullOrWhiteSpace(txtFirstName.Text))
+            {
+                MessageBox.Show("First Name is required.", "Required", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                txtFirstName.Focus();
+                return false;
+            }
+
+            // Last Name
+            if (string.IsNullOrWhiteSpace(txtLastName.Text))
+            {
+                MessageBox.Show("Last Name is required.", "Required", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                txtLastName.Focus();
+                return false;
+            }
+
+            // Email
+            if (string.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                MessageBox.Show("Email is required.", "Required", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                txtEmail.Focus();
+                return false;
+            }
+
+            // Email format check
+            try
+            {
+                var email = new System.Net.Mail.MailAddress(txtEmail.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Enter a valid email address.", "Check", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                txtEmail.Focus();
+                return false;
+            }
+
+            // Date of Birth (basic check)
+            if (dtpDob.Value >= DateTime.Now)
+            {
+                MessageBox.Show("Date of Birth must be in the past.", "Check", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                return false;
+            }
+
+            // Radio buttons (New / Existing)
+            if (!radioMale.Checked && !radioFemale.Checked)
+            {
+                MessageBox.Show("Please select Male or Female.", "Required", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                return false;
+            }
+            // Cell validation
+            if (string.IsNullOrWhiteSpace(txtCell.Text) || txtCell.Text == "(876)555-5555")
+            {
+                MessageBox.Show("Phone number is required.", "Required", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                txtCell.Focus();
+                return false;
+            }
+            // Ensure only digits and max 10 characters
+            if (!txtCell.Text.All(char.IsDigit) || txtCell.Text.Length != 10)
+            {
+                MessageBox.Show("Phone number must be digits, 10 only.", "Incorrect", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                txtCell.Focus();
+                return false;
+            }
+            // Mobile validation
+            if (string.IsNullOrWhiteSpace(txtMobile.Text) || txtMobile.Text == "(876)555-5555")
+            {
+                MessageBox.Show("Phone number is required.", "Required", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                txtMobile.Focus();
+                return false;
+            }
+            // Ensure only digits and max 10 characters
+            if (!txtCell.Text.All(char.IsDigit) || txtCell.Text.Length != 10)
+            {
+                MessageBox.Show("Phone number must be digits, 10 only.", "Incorrect", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                txtCell.Focus();
+                return false;
+            }
+
+            // Ensure only digits and max 10 characters
+            if (!txtMobile.Text.All(char.IsDigit) || txtMobile.Text.Length > 10)
+            {
+                MessageBox.Show("Phone number must be digits, 10 only.", "Incorrect", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                txtMobile.Focus();
+                return false;
+            }
+
+            // Parish validation
+            if (cmbParish.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please select a parish.", "Required", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                cmbParish.Focus();
+                return false;
+            }
+
+            // Address validation
+            if (string.IsNullOrWhiteSpace(txtAddress.Text))
+            {
+                MessageBox.Show("Address is required.", "Required", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                txtAddress.Focus();
+                return false;
+            }
+
+            // Town validation
+            if (string.IsNullOrWhiteSpace(txtTown.Text))
+            {
+                MessageBox.Show("Town is required.", "Required", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                txtTown.Focus();
+                return false;
+            }
+
+            return true; // ✅ everything passed
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtEmail.Clear();
+            txtCell.Text = "(876)555-5555";
+            txtCell.ForeColor = Color.LightGray;
+            txtMobile.Text = "(876)555-5555";
+            txtMobile.ForeColor = Color.FromArgb(220, 220, 220);
+            radioFemale.Checked = false;
+            radioMale.Checked = false;
+            dtpDob.Value = DateTime.Now.AddYears(-18);
+            txtAddress.Clear();
+            txtTown.Clear();
+            cmbParish.SelectedIndex = 0;
+        }
+
+        private void btnNewAppt_Click(object sender, EventArgs e)
+        {
+            FrmNewAppt newAppointmentForm = new FrmNewAppt();
+            newAppointmentForm.ShowDialog();
+        }
+
+        private void btnNewUser_Click(object sender, EventArgs e)
+        {
+            if (!ValidateForm()) return;
+            string first = txtFirstName.Text;
+            string last = txtLastName.Text;
+
+            try
+            {
+                SaveUser();
+                MessageBox.Show("User Created: " + first + " " + last, "Welcome", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error creating user: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtCell_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Allow digits and backspace
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtMobile_Enter(object sender, EventArgs e)
+        {
+            if (txtMobile.Text == "(876)555-5555")
+            {
+                txtMobile.Text = "";
+                txtMobile.ForeColor = Color.Black;
+            }
+        }
+        private void txtMobile_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMobile.Text))
+            {
+                txtMobile.Text = "(876)555-5555";
+                txtMobile.ForeColor = Color.FromArgb(220, 220, 220);
+            }
+        }
+        private void txtCell_Enter(object sender, EventArgs e)
+        {
+            if (txtCell.Text == "(876)555-5555")
+            {
+                txtCell.Text = "";
+                txtCell.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtCell_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCell.Text))
+            {
+                txtCell.Text = "(876)555-5555";
+                txtCell.ForeColor = Color.FromArgb(220, 220, 220);
+            }
+        }
+        private void SaveUser()
+        {
+            //todo: add database logic here
+        }
+
+        private void FrmPortal_Load(object sender, EventArgs e)
+        {
+            cmbParish.Items.Insert(0, "Select a Parish.");
+            cmbParish.SelectedIndex = 0;
+
+            // Set placeholder for Cell
+            txtCell.Text = "(876)555-5555";
+            txtCell.ForeColor = Color.FromArgb(220, 220, 220);
+
+            // Set placeholder for Mobile
+            txtMobile.Text = "(876)555-5555";
+            txtMobile.ForeColor = Color.FromArgb(220, 220, 220);
+        }
+    }
+    
+}
